5a03241c05f456507adb965b236892db401e9adc
Andres Cardenas <andrescardenase77@gmail.com>
2026-05-18 18:43:48 -0500

    ws

---
+++ php_practice/index.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Hello PHP</h1>";
+echo "<p>PHP is working!!!!</p>";
+// This is a comment in PHP
+/* This is a multi-line comment in PHP
+   It can span multiple lines 
+*/
+$programmer_name = "Andres Cardenas";
+?>
